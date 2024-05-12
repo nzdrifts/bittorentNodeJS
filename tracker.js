9904e5056dfc90c07907aabce848b2a8ef8c4f22
Kirill Toptun <kirilltoptun@gmail.com>
2024-05-12 21:06:10 +1200

    added respType function to tracker.js

--- tracker.js
+++ tracker.js
@@ -115,3 +115,9 @@ function parseAnnounceResp(resp) {
       })
     }
   }
+
+  function respType(resp) {
+    const action = resp.readUInt32BE(0);
+    if (action === 0) return 'connect';
+    if (action === 1) return 'announce';
+  }
