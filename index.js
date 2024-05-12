1f72ea9b90b070b31452debb854882e3bd500ff9
Kirill Toptun <kirilltoptun@gmail.com>
2024-05-12 14:25:38 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const fs = require('fs');
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+console.log(torrent.announce.toString('utf8'));
+
